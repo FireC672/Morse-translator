16f61bc8b9843fd7e30c5bf0f5a4c5dacfd4db16
FireC657 <comptagedepoints@gmail.com>
2023-05-23 14:23:34 +0100

    Tested if all components worked, now let us work on the real deal.

--- main.c
+++ main.c
@@ -4,10 +4,6 @@
 #include "map.h"
 
 int main(int argc, char** argv){
-    map_head_t* head = initializeTable();
-    push_element(head, createHead('B',"Hello"));
-    map_element_t* p = findElementByKey('A',head);
 
-    printf("%s",p->value);
     return 0;
 }
