a0cc429634023ff820959286bc91478c81248241
FireC657 <comptagedepoints@gmail.com>
2023-05-23 14:20:52 +0100

    Struggled with the compiler, but where are here . Removed the files from the respective directories for a while. At the end I will put them back.

--- main.c
+++ main.c
@@ -1,6 +1,15 @@
 #include <stdio.h> 
 #include <stdlib.h> 
+#include "morse.h"
+#include "map.h"
 
 int main(int argc, char** argv){
+    map_head_t* head = initializeTable();
+    push_element(head, createHead('B',"Hello"));
+    map_element_t* p = findElementByKey('A',head);
+
+    printf("%s",p->value);
+
+    deconstructMap(head);
     return 0;
 }
