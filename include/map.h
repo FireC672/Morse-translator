4e76fd92058b3bff8aef00a896973afb26bede32
FireC657 <comptagedepoints@gmail.com>
2023-05-23 14:13:46 +0100

    Initial commit Well actually i did start working on it, but i decided that i needed to track my changes

---
+++ include/map.h
@@ -0,0 +1,20 @@
+#ifndef MAP_UTILITAR_INCLUDED 
+#define MAP_UTILITAR_INCLUDED 
+
+// This is for hashmaps.
+
+typedef struct {
+  int key; 
+  char* value;
+  map_element_t* next;
+
+} map_element_t;
+
+typedef map_element_t map_head_t; 
+
+map_head_t* createHead(int _key, char* _value);
+int push_element(map_head_t* h, map_element_t* elem);
+map_element_t* findElementByKey(int _key, map_head_t *head);
+void deconstructMap(map_head_t* head);
+
+#endif 
